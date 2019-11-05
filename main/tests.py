a9f300827d9912f8b2513ef5bd42e8e216ff2cce
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-05 11:13:34 +0000

    Adding test to main test file to check successful load of index, test passed

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here    
+def test_indexpage(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
