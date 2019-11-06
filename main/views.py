511244c33ee474efe891f3afebc303e8a14920b4
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-06 18:34:38 +0000

    Adding messaging functionality in base template using django built in messaging

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-
+    messages.success(request, f'You have loaded the landing page')
     return render(request, 'index.html') 
