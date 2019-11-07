681e562f64c99c419b8e43fe961dbdf80a048d39
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-07 10:14:56 +0000

    Adding testcase to users/test.py to test registration form submit

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from users.forms import UserRegisterForm
 
-# Create your tests here.
+
+class UserTests(TestCase):
+    # testing registration form accepts valid data
+    def test_forms(self):
+        form_data = {'username': 'djangotest',
+        'email': 'test@test.com',
+        'password1': 'm1234rthq',
+        'password2': 'm1234rthq',}
+        form = UserRegisterForm(data=form_data)
+        self.assertTrue(form.is_valid())
