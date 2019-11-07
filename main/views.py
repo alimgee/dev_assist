75d87a9bc18afc8c14e5973327f4c7ad2427134c
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-07 16:42:06 +0000

    updating home view to message user when they are logged in and as who

--- main/views.py
+++ main/views.py
@@ -4,7 +4,10 @@ from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    messages.success(request, f'You have loaded the landing page')
+    if request.user.is_authenticated:
+        username = request.user.username
+        messages.success(request, f'You are currently logged in as {username}')
+        
     return render(request, 'index.html', {'title': 'Home'}) 
 
 def about(request):
