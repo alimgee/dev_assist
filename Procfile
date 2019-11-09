8811c728d192ebe3e3ca4ae3c5773424c2c9df15
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-09 10:36:34 +0000

    Committing for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dev_assist.wsgi:application
--- dev_assist/settings.py
+++ dev_assist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url # adding for prodiction postgres
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,12 +78,25 @@ WSGI_APPLICATION = 'dev_assist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+#}
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    print("Postgres DB not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    }
 
 
 # Password validation
