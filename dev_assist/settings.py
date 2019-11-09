02e2552dc0d6df1627e916ee5033e50ff947c4f0
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-09 11:37:27 +0000

    Modidying settings to troubleshoot css not sisplaying in Heroku

--- dev_assist/settings.py
+++ dev_assist/settings.py
@@ -134,12 +134,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static") # setting default directories for static files
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # setting default location of /media folder
 MEDIA_URL = '/media/'
 
