f2ba8e6b2e7f7131d1cc09d67492f298a3a99bf5
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-09 13:18:13 +0000

    Updaing forum.model with default=1 for user foreign key to see if it fixes travis error

--- forum/models.py
+++ forum/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # cascade -  when user is deleted remove all posts
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1) # cascade -  when user is deleted remove all posts
 
     def __str__(self):
         return self.title # will return the title in the shell view
@@ -21,7 +21,7 @@ class Comment(models.Model):
     content = models.TextField(max_length=1500, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     query = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
-    comment_by = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+    comment_by = models.ForeignKey(User,null=True,on_delete=models.CASCADE, default=1)
     comment_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
