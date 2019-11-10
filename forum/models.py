690291f2e84f24b108d9947c57b0d7c900716a9f
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-10 16:53:39 +0000

    Adjusting post module to remove get_wabsolute_url as its no longer needed

--- forum/models.py
+++ forum/models.py
@@ -11,11 +11,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1) # cascade -  when user is deleted remove all posts
 
     def __str__(self):
-        return self.title # will return the title in the shell view
-    
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
-    
+        return self.title # will return the title in the shell view    
+
+  
 class Comment(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=1500, null=True)
