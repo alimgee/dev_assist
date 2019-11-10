9dda79a782a9c57f25d8221c465ec6d1ff434136
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-10 11:29:24 +0000

    Adding unit test for loading of /community

--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from forum.views import PostListView
 
-# Create your tests here.
+
+# Test to check the forum
+class TestPost(TestCase):
+    # testing loading of forum landing page
+    def test_forum_landing_page(self):
+        response = self.client.get('/community/')
+        self.assertEqual(response.status_code, 200)
+)
