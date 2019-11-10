cdf5a91dc14b699e5af54bb6cc4ec6ea964b59ff
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-10 15:58:11 +0000

    rewriting class base views for forun /comunity and query detail to see if class based views is causing the travis issue

--- forum/urls.py
+++ forum/urls.py
@@ -3,8 +3,10 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='posts'),
-    path('query/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    #path('', PostListView.as_view(), name='posts'),
+    path('', views.community, name='posts'),
+    #path('query/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('query/<int:pk>/', views.query_detail, name='post-detail'),
     path('query/new/', PostCreateView.as_view(), name='post-create'),
     path('query/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, get_object_or_404
 from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
@@ -10,6 +10,24 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     
+# creating community page view using functions instead
+def community(request):
+    querys = Post.objects.all()
+    context = {
+        "posts": querys
+    }
+    return render(request, "forum/forum_list.html", context)
+
+# creating query detail page view using functions instead
+def query_detail(request, pk):
+    query_detail = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.all()
+    context = {
+        "post": query_detail,
+        "comments":comments,
+
+    }
+    return render(request, "forum/post_detail.html", context)
 
 class PostDetailView(DetailView):
     model = Post 
@@ -25,6 +43,7 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content']
+    success_url = '/community/'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -42,4 +61,25 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         if self.request.user == post.author:
             return True
-        return False
+        else:
+            return reverse('home')
+
+
+def post_edit(request, pk):
+    """ Edit a Single Ticket """
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        ticket_form = TicketForm(request.POST, instance=ticket)
+        if ticket_form.is_valid():
+            ticket_form.instance.date_edited = timezone.now()
+            ticket_form.save()
+            messages.success(
+                request, f"Ticket successfully updated!")
+            return redirect(tickets_view_one, ticket.pk)
+    else:
+        ticket_form = TicketForm(instance=ticket)
+    context = {
+        "ticket": ticket,
+        "ticket_form": ticket_form,
+    }
+    return render(request, "tickets_edit.html", context)
