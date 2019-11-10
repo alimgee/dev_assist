5b28fa48284421317a5d545dc7711f8cbed64427
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-10 13:07:12 +0000

    Adding update query function

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
     path('query/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('query/new/', PostCreateView.as_view(), name='post-create'),
+    path('query/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from .models import Post, Comment
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 # Create view for showing forum posts
 class PostListView(ListView):
@@ -28,3 +29,17 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):# tests that user owns post using built in mixin
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
