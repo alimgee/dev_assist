11bc75db7bf632865c175de65ea7b23aa3c453e7
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-20 10:32:51 +0000

    Modifying forum model to try to resolve Traivis issue

--- forum/models.py
+++ forum/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1) # cascade -  when user is deleted remove all posts
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # cascade -  when user is deleted remove all posts
 
     class Meta:
         ordering = ("-date_posted", )
