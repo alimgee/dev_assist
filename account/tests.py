36d2ec1b3183a332f793e484cd8864b2dab44a39
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-24 17:31:10 +0000

    Adding tests for account app pages load to account/tests.py and modifying forum landing page load test also

--- account/tests.py
+++ account/tests.py
@@ -2,7 +2,21 @@ from django.test import TestCase
 from account.forms import UserRegisterForm
 
 
-class UserTests(TestCase):
+class UserTests(TestCase):  
+
+    # testing load of pages in account app
+    def test_register_page_load(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_load(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_logoit_page_load(self):
+        response = self.client.get('/logout/')
+        self.assertEqual(response.status_code, 200)
+    
     # testing registration form accepts valid data
     def test_forms(self):
         form_data = {'username': 'djangotest',
--- forum/tests.py
+++ forum/tests.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
-from forum.views import PostListView
 
 
-# Test to check the forum
-class TestPost(TestCase):
+# Tests to check the forum
+class TestForum(TestCase):
     # testing loading of forum landing page
     def test_forum_landing_page(self):
         response = self.client.get('/community/')
         self.assertEqual(response.status_code, 200)
-)
