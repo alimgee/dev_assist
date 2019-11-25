10a2f985e570b38d514ef5b9d157bbad3d5a2bef
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-25 12:29:16 +0000

    Adding some styling to veiw cart page

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 
 def view_cart(request):
-    """A View that renders the cart contents page"""
-    return render(request, "cart/cart.html")
+    '''A View that renders the cart contents page'''
+    context={
+        'title':'Cart'
+    }
+    return render(request, "cart/cart.html", context)
