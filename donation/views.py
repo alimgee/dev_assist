6e6eec99c85dc78698e17c05b3862cece37d6753
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-25 11:24:56 +0000

    Adding donation items to db and adjusting donation.html to show items

--- donation/views.py
+++ donation/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from donation.models import Donation
 
 # Create your views here.
 def donations(request):
+    donations = Donation.objects.all()
+    context = {
         
-    return render(request, 'donation/donation.html') 
+        "donations": donations
+    } 
+    return render(request, 'donation/donation.html', context , {'title': 'Donation'}) 
