bdc3d99b625db4e686edc64ea845cc4a24c99f9c
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-25 17:34:20 +0000

    Adding in view to cart to allow deletion of items from the cart

--- cart/views.py
+++ cart/views.py
@@ -16,3 +16,13 @@ def add_to_cart(request, id):
     request.session['cart'] = cart
     return redirect(reverse('view-cart'))
 
+def delete_from_cart(request, id):
+    '''function to remove an item from the cart'''
+    if request.method == 'POST':
+        item_id = id
+        print(f'itemid is{item_id}')
+        cart = request.session.get('cart', {})
+        if cart[item_id] > 0:
+            cart.pop(item_id)
+            request.session['cart'] = cart
+    return redirect(reverse('view-cart'))
