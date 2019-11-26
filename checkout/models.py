2268069c9bd803ef0399c4ac95899915ed8eb6cb
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-26 16:56:09 +0000

    Adding order line model for checkout to models.py

--- checkout/models.py
+++ checkout/models.py
@@ -6,9 +6,17 @@ from donation.models import Donation
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
-    email = models.EmailField(max_length=250)
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE) 
     date = models.DateField()
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    donation = models.ForeignKey(Donation, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.donation.name, self.donation.price)
