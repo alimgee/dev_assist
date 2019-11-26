b297cba7e66d4961474b481c9d6d501618e43ac9
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-26 14:20:24 +0000

    Adding model for  checkout Order

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User # built in django user table
+from donation.models import Donation
 
 # Create your models here.
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    email = models.EmailField(max_length=250)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE) 
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
