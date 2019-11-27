f5ef394dbffaed91a99f374976ce0a9a67c0e32b
Alan Mc Gee <mcgee.al@gmail.com>
2019-11-27 14:49:03 +0000

    Adding form checking to check view to proccess credit card

--- checkout/views.py
+++ checkout/views.py
@@ -14,7 +14,51 @@ stripe.api_key = settings.STRIPE_SECRET
 
 
 def checkout(request):
-    payment_form = MakePaymentForm()
-    order_form = OrderForm()
+    if request.method=="POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+        
+        if order_form.is_valid() and payment_form.is_valid():
+            
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+            
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                donation = get_object_or_404(Donation, pk=id)
+                total += quantity * donation.price
+                order_line_item = OrderLineItem(
+                    order = order, 
+                    donation = product, 
+                    quantity = quantity
+                    )
+                order_line_item.save()
+                
+            try:
+                customer = stripe.Charge.create(
+                    amount = int(total * 100),
+                    currency = "EUR",
+                    description = request.user.email,
+                    card = payment_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+                
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('donations'))
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
         
     return render(request, "checkout/checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
+
+
