dc3a35d893ac4278ca28100a1c47e620196e4e16
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-02 14:52:41 +0000

    Adjusting account registration form to check for email existing email addresses

--- account/forms.py
+++ account/forms.py
@@ -8,3 +8,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta: # additional metadata for form
         model = User # using built in User model
         fields = ['username', 'email', 'password1', 'password2'] # addtional fields to user model
+
+    def clean_email(self):
+        '''
+        Checking for emails addresses that may already exist
+        '''
+        email = self.cleaned_data.get("email")
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(
+                f"A user with that email address already exists.")
+        return email
