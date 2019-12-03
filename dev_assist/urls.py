9a22b6ed1f420c3ec6f80e266b13bde3e3006284
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-03 12:39:03 +0000

    Adding password done and complete routes and templates, renaming password templates

--- dev_assist/urls.py
+++ dev_assist/urls.py
@@ -40,8 +40,18 @@ urlpatterns = [
          name='password_reset'),
      path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'
+             template_name='account/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='account/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='account/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 
 ]
