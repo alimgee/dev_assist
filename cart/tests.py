1f989c663f29245887d6e4f94dedd6fc776b5355
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-04 11:56:46 +0000

    Adding unittest to  cart test.py to check user can only load cart if logged in

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from account.forms import UserRegisterForm
 
-# Create your tests here.
+class CartTestsNotLoggedIn(TestCase):  
+    '''
+    testing cart page doesnt load when logged out
+    (should return a 302 as the app should redirect to login page)
+    '''
+    def test_cart_page_load(self):
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 302)
+
+class CartTestsLoggedIn(TestCase):  
+
+    # setting up logged in user
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+        self.client.post('/login/', self.credentials, follow=True)
+    # testing cart page load when logged in
+    def test_cart_page_load(self):
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 200)
+    
