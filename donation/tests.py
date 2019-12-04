4d7f6fcd9e4b7a00c3b6c22dc48829c30106f01a
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-04 12:24:00 +0000

    Adding unit tests to donation/test.py, checking page load and templates used

--- donation/tests.py
+++ donation/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+class UserTests(TestCase):  
+
+    # testing load of donate page
+    def test_donate_page_load(self):
+        response = self.client.get('/donate/')
+        self.assertEqual(response.status_code, 200)
+
+    # testing load of relevant templates to donate page
+    def test_donate_page_templates_load(self):
+        response = self.client.get('/donate/')
+        self.assertTemplateUsed(response, "donation/donation.html")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "donation/includes/intro.html")
