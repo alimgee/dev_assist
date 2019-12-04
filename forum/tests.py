951aebeb0e074bf1be5a7f1582d9e097f4013037
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-04 12:47:14 +0000

    Adding tests to forum/tests.py , checking for pageload and templates used

--- forum/tests.py
+++ forum/tests.py
@@ -7,3 +7,10 @@ class TestForum(TestCase):
     def test_forum_landing_page(self):
         response = self.client.get('/community/')
         self.assertEqual(response.status_code, 200)
+
+    # testing load of relevant templates to community page
+    def test_forum_page_templates_load(self):
+        response = self.client.get('/community/')
+        self.assertTemplateUsed(response, "forum/forum_list.html")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "forum/includes/intro.html")   
