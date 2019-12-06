b3918d0b4bb1b8401b9e2029969b38574c7950b4
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-06 13:21:44 +0000

    Running collectstatic

--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from account.forms import UserRegisterForm
 
-# Create your tests here.
+
+class CartTestsLoggedIn(TestCase):
+
+    # setting up logged in user
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+        self.client.post('/login/', self.credentials, follow=True)
+
+    # testing checkout page load when logged in
+    def test_cart_page_load(self):
+        response = self.client.get('/checkout/')
+        self.assertEqual(response.status_code, 200)
