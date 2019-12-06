faec257988dcd1fb5143ae3eae6b87fda3847b6b
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-06 12:56:54 +0000

    Running account/tests.py through PEP8 validator

--- account/tests.py
+++ account/tests.py
@@ -3,7 +3,7 @@ from account.forms import UserRegisterForm
 from django.contrib.auth.models import User
 
 
-class UserTests(TestCase):  
+class UserTests(TestCase):
 
     # testing load of pages in account app
     def test_register_page_load(self):
@@ -13,11 +13,11 @@ class UserTests(TestCase):
     def test_login_page_load(self):
         response = self.client.get('/login/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_logout_page_load(self):
         response = self.client.get('/logout/')
         self.assertEqual(response.status_code, 200)
-    
+
     def test_register_page_templates_load(self):
         response = self.client.get('/register/')
         self.assertTemplateUsed(response, "register.html")
@@ -42,17 +42,18 @@ class UserTests(TestCase):
             'username': 'testuser',
             'password': 'secret'}
         User.objects.create_user(**self.credentials)
+
     def test_login(self):
         # send login data
         response = self.client.post('/login/', self.credentials, follow=True)
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
-    
+
     # testing registration form accepts valid data
     def test_forms(self):
         form_data = {'username': 'djangotest',
-        'email': 'test@test.com',
-        'password1': 'm1234rthq',
-        'password2': 'm1234rthq',}
+                     'email': 'test@test.com',
+                     'password1': 'm1234rthq',
+                     'password2': 'm1234rthq'}
         form = UserRegisterForm(data=form_data)
         self.assertTrue(form.is_valid())
