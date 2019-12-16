92b3cdfc9155bfd74d341e233ce7cdc4dfa6329d
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-16 12:48:46 +0000

    Passing donation/views.py throught PEP8 validator

--- donation/views.py
+++ donation/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from donation.models import Donation
 
-# Create your views here.
+
+# view for donation page
 def donations(request):
+    '''
+    function to get all donation objects in db
+    and pass to template for display
+    '''
     donations = Donation.objects.all()
     context = {
-        
         "donations": donations,
-        "title" : 'Donations'
-    } 
-    return render(request, 'donation/donation.html', context ) 
+        "title": 'Donations'
+    }
+    return render(request, 'donation/donation.html', context)
