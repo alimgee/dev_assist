2f97af4ed94c2b26792485266a6816bc92453955
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-16 11:26:58 +0000

    Passing main/views.py through pep8 validator

--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.contrib import messages
 
-# Create your views here.
 
+# functions to load landing page and about page
 def home(request):
     if request.user.is_authenticated:
         username = request.user.username
-        messages.success(request, f'You are currently logged in as {username}')
-        
-    return render(request, 'index.html', {'title': 'Home'}) 
+        messages.success(request,
+                         f'You are currently logged in as {username}')
+
+    return render(request, 'index.html', {'title': 'Home'})
+
 
 def about(request):
-    messages.success(request, f'You have loaded the about page')
-    return render(request, 'about.html',{'title': 'About'}) 
+
+    return render(request, 'about.html', {'title': 'About'})
