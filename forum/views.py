d8617ddab552ab2e06511e5c1c4ee23b2f848d17
Alan Mc Gee <mcgee.al@gmail.com>
2019-12-16 13:43:55 +0000

    Passing forum/views.py through PEP8 validator

--- forum/views.py
+++ forum/views.py
@@ -5,11 +5,11 @@ from forum.forms import QueryForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
-from django.db.models import Q #for multiple searchs
+from django.db.models import Q  # for multiple searchs
 
-    
+
 def community(request):
-    ''' 
+    '''
     Function to load the forum landing page
     at /community and display any current posts
     in the db
@@ -23,19 +23,20 @@ def community(request):
     # passing current page to context
     post_pag = paginator.get_page(page)
     context = {
-        
         "posts": post_pag
     }
-    return render(request, "forum/forum_list.html", context)
+    return render(request,
+                  "forum/forum_list.html", context)
 
 
 def do_search(request):
     query = request.GET.get('q')
-    posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query)) 
+    posts = Post.objects.filter(Q(title__icontains=query) |
+                                Q(content__icontains=query))
     if not posts:
-        messages.warning(request, f'Your serach for "{query} " returned no results')
+        messages.warning(request,
+                         f'Your serach for "{query} " returned no results')
     context = {
-        
         "posts": posts
     }
     return render(request, "forum/forum_list.html", context)
@@ -64,24 +65,24 @@ def query_detail(request, pk):
         # take submitted form
         comment_form = CommentForm(request.POST or None)
         # if submitted form is valid save it to db
-        if comment_form.is_valid():         
+        if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             # link query foregin key
             comment.query = query_detail
-            # adding detail to title field
+            # adding query detail to title field
             comment.title = query_detail.title
             # adding user to foreign key field
             comment.comment_by = user
             comment.save()
             # message user and reload post detail page
             messages.success(request, f'Comment added sucessfully')
-            return redirect('post-detail', pk = query_detail.pk)
+            return redirect('post-detail', pk=query_detail.pk)
     # finding comments related to current post
-    comments = Comment.objects.filter(query_id = pk )
+    comments = Comment.objects.filter(query_id=pk)
     # adding to context and returning to post detail page
     context = {
         "post": query_detail,
-        "comments":comments,
+        "comments": comments,
         "form": comment_form
 
     }
@@ -90,33 +91,38 @@ def query_detail(request, pk):
 
 @login_required
 def create_query(request):
-     '''
-     function to create a post
-     '''
-     user = request.user
-     # if form is submitted
-     if request.method == "POST":
-         # getting form
-         query_form = QueryForm(request.POST)
-         # if form is valid save and message
-         if query_form.is_valid():
-             query = query_form.save(commit=False)
-             query.author = user
-             query.save()
-             messages.success(request, f'Thanks for submiting your ticket')
-             return redirect('posts')
-     else:
-         # loading form for psots
-         query_form = QueryForm()
-     # passing form to context and loading page
-     context = {
-         "form": query_form,
-     }
-     return render(request, "forum/post_form.html", context)
+    '''
+    function to create a post and add
+    to the db
+    '''
+    user = request.user
+    # if form is submitted
+    if request.method == "POST":
+        # getting form
+        query_form = QueryForm(request.POST)
+        # if form is valid save and message
+        if query_form.is_valid():
+            query = query_form.save(commit=False)
+            query.author = user
+            query.save()
+            messages.success(request, f'Thanks for submiting your ticket')
+            return redirect('posts')
+    else:
+        # loading form for posts
+        query_form = QueryForm()
+    # passing form to context and loading page
+    context = {
+        "form": query_form,
+    }
+    return render(request, "forum/post_form.html", context)
+
 
 @login_required
 def edit_query(request, pk):
-    ''' Function to allow a user to edit their own query'''
+    '''
+    Function to allow a user to edit their
+    own query
+    '''
     query = get_object_or_404(Post, pk=pk)
     logged_user = request.user.id
     author = query.author.id
@@ -127,7 +133,6 @@ def edit_query(request, pk):
     if request.method == "POST":
         query_form = QueryForm(request.POST, instance=query)
         if query_form.is_valid():
-            #query_form.instance.date_posted = timezone.now()
             query_form.save()
             messages.success(
                 request, f"You have successfully changed your query")
@@ -139,19 +144,25 @@ def edit_query(request, pk):
     }
     return render(request, "forum/edit_form.html", context)
 
+
 @login_required
 def delete_query(request, pk):
     '''
     Function to allow Query owner to delete their post
     '''
     query = get_object_or_404(Post, pk=pk)
+
+    # getting current logged in user
     logged_user = request.user.id
+    # getting original author of post
     author = query.author.id
 
+    # if current user is not the author of the post dont allow deletion
     if logged_user is not author:
-        messages.warning(request, f'You are not the current owner of this query')
+        messages.warning(request,
+                         f'You are not the current owner of this query')
         return redirect('posts')
-
+    # delete post and message user
     query.delete()
     messages.success(
         request, f"Your Post has now been removed")
