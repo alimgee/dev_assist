be424fd54c49ee50cebaf9d582a61a2addc5b583
Alan Mc Gee <mcgee.al@gmail.com>
2020-01-10 12:26:10 +0000

    Updating text content on Add Post form

--- forum/forms.py
+++ forum/forms.py
@@ -5,13 +5,13 @@ from forum.models import Post, Comment
 class QueryForm(forms.ModelForm):
     '''form for sbmitting a new query'''
     title = forms.CharField(
-        label="Your Query Title",
+        label="Your Post Title",
         min_length=3,
         max_length=80,
         widget=forms.TextInput(),
         required=True)
     content = forms.CharField(
-        label="Query Detail",
+        label="Post Detail",
         min_length=10,
         max_length=1500,
         widget=forms.Textarea(),
